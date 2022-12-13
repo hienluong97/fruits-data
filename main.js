9dcb4d9ab82ccc76342cfe050c03e62d31427eb8
hienluong <hienluong1997@gmail.com>
2022-12-13 14:17:05 +0700

    add auth

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
 // Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares)
+server.use(middlewares);
+
+// /!\ Bind the router db to the app
+server.db = router.db;
 
 // Add custom routes before JSON Server router
-server.get('/echo', (req, res) => {
-  res.jsonp(req.query)
-})
+server.get("/echo", (req, res) => {
+  res.jsonp(req.query);
+});
+
+// You must apply the auth middleware before the router
+server.use(auth);
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
+server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    req.body.createdAt = Date.now()
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
   }
   // Continue to JSON Server router
-  next()
-})
+  next();
+});
 
 // Use default router
 const PORT = process.env.PORT || 3000;
-server.use(router)
+server.use(router);
 server.listen(PORT, () => {
-  console.log('JSON Server is running')
-})
+  console.log("JSON Server is running");
+});
